7b4cd2ba4f10ca9ea6469d556b9ef6e0ea106fac
Aman Talwar <atalwar@xilinx.com>
2020-09-11 17:43:04 +0530

    submit

--- _ext/edit_on_github.py
+++ _ext/edit_on_github.py
@@ -13,14 +13,14 @@ __licence__ = 'BSD (3 clause)'
 
 
 def get_github_url(app, view, path):
-    return 'https://github.com/{project}/{view}/{branch}/{path}'.format(
+    return 'https://gitenterprise.xilinx.com/{project}/{view}/{branch}/{path}'.format(
         project=app.config.edit_on_github_project,
         view=view,
         branch=app.config.edit_on_github_branch,
         path=path)
 
 def get_github_feedback(app, view, path):
-    return 'https://github.com/{project}/{view}/{branch}/{path}'.format(
+    return 'https://gitenterprise.xilinx.com/{project}/{view}/{branch}/{path}'.format(
         project=app.config.edit_on_github_project,
         view=app.config.feedback_on_github_url,
         branch=app.config.edit_on_github_branch,
--- conf.py
+++ conf.py
@@ -68,7 +68,7 @@ autosectionlabel_prefix_document = True
 
 
 # Configure 'Edit on GitHub' extension
-edit_on_github_project = 'amantalwar04/testversions'
+edit_on_github_project = 'atalwar/testversions'
 edit_on_github_branch = 'master'
 feedback_on_github_url = 'issues/new?labels=documentation&title=[Feedback]+'
 
@@ -233,3 +233,52 @@ rst_epilog = """
 #            'auto_toc_tree_section': 'Contents',
 #            }, True)
 #    app.add_transform(AutoStructify)
+
+
+############################
+# SETUP THE RTD LOWER-LEFT #
+############################
+try:
+   html_context
+except NameError:
+   html_context = dict()
+html_context['display_lower_left'] = True
+
+templates_path = ['_templates']
+ 
+# SET CURRENT_LANGUAGE
+if 'current_language' in os.environ:
+   # get the current_language env var set by buildDocs.sh
+   current_language = os.environ['current_language']
+else:
+   # the user is probably doing `make html`
+   # set this build's current language to english
+   current_language = 'en'
+ 
+# tell the theme which language to we're currently building
+html_context['current_language'] = current_language
+ 
+# SET CURRENT_VERSION
+from git import Repo
+repo = Repo( search_parent_directories=True )
+ 
+if 'current_version' in os.environ:
+   # get the current_version env var set by buildDocs.sh
+   current_version = os.environ['current_version']
+else:
+   # the user is probably doing `make html`
+   # set this build's current version by looking at the branch
+   current_version = repo.active_branch.name
+ 
+# tell the theme which version we're currently on ('current_version' affects
+# the lower-left rtd menu and 'version' affects the logo-area version)
+html_context['current_version'] = current_version
+html_context['version'] = current_version
+ 
+# POPULATE LINKS TO OTHER VERSIONS
+html_context['versions'] = list()
+ 
+versions = [branch.name for branch in repo.branches]
+for version in versions:
+   html_context['versions'].append( (version, '/' +edit_on_github_project+ '/' +version+ '/html/README.html') )
+ 
